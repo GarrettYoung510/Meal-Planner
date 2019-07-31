7d5c0ddfd44b56dabf32d57df1914d64743609a1
Denzel <denzelhartshorn@gmail.com>
2019-07-31 13:02:09 -0700

    ui components

---
+++ client/src/components/Partials/Card/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card mt-5">
+      <h5 className="card-header" />
+      <div className="card-body">
+        <h5 className="card-title" />
+        <hr />
+        <p className="card-text" />
+        <a href="#" className="btn btn-primary" />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ client/src/components/Partials/Jumbotron/Jumbotron.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Jumbotron = props => {
+  return <div className="jumbotron">{props.children}</div>;
+};
+
+export default Jumbotron;
