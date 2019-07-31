243a3ddad7f3fbc1ae0508edf039e20502b9b3c3
Ryan Gelow <exisphere@gmail.com>
2019-07-31 14:56:54 -0700

    sign up form updated with all inputs, still needs formatting

--- client/src/components/Partials/Card/Card.js
+++ client/src/components/Partials/Card/Card.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Card = () => {
+const Card = props => {
     return (
         <div className="card">
-            <h5 className="card-header"></h5>
+            <h5 className="card-header">{props.children}</h5>
             <div className="card-body">
-                <h5 className="card-title"></h5>
-                <p className="card-text"></p>
-                <a href="#" className="btn btn-primary"></a>
+                <h5 className="card-title">{props.children}</h5>
+                <p className="card-text">{props.children}</p>
+                <a href={props.href} className="btn btn-primary">{props.children}</a>
             </div>
         </div>
     )
