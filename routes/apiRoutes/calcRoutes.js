288b99c81d02bb24ddae4a9765b90b6a3c03e020
Shantell Davis <shantell87@gmail.com>
2019-08-02 10:39:09 -0700

    updated calcController with auth parameters, for user signup

--- routes/apiRoutes/calcRoutes.js
+++ routes/apiRoutes/calcRoutes.js
@@ -1,14 +1,13 @@
 const router = require('express').Router();
 const calcController = require("./../../controllers/calcControllers");
 
-// const passportService = require('./../../services/passport');
-// const authMiddleware = require('./../../middlewares/authMiddlewares');
+const passportService = require('./../../services/passport');
+const authMiddleware = require('./../../middlewares/authMiddlewares');
 
 
 // /api/calc
 
 router.route('/')
-.get(calcController.getBMR)
-//.post(authMiddleware.requireAuth, calcController.createCalc)
+.get(authMiddleware.requireAuth, calcController.getBMR)
 
 module.exports = router;
