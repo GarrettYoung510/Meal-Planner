6f27e76669999213b5ffb831f38a59a4f80f6fcd
Garrett Young <garrettyoung510@gmail.com>
2019-08-02 11:37:57 -0700

    got API sending from backend to frontend and being able to select

--- client/src/components/Pages/MealSelect/index.js
+++ client/src/components/Pages/MealSelect/index.js
@@ -9,35 +9,54 @@ import Signin from "../../../containers/Signin";
 import Signup from "../../../containers/Signup";
 import Column from "../../Partials/Column/Column";
 import MainCourseDropdown from "../../Partials/MainCourseDropDown/index";
+import { type } from "os";
 
 class MealSelect extends Component {
+
+  state = {
+    // pull the initial meals to select
+    mealsArray : ['meal 1', 'meal 2']
+  }
+
+  handleMealSelect = (value) => {
+    console.log(value);
+    this.getMeals(value);
+  }
+
+  // handleMealSelect(value){
+  //   console.log(value);
+  //   // this.getMeals(value);
+  //   console.log(this)
+  // }
+
   componentDidMount() {
     // // mealdb queries
     // this.getRandomMeal();
-    this.getMealOfType();
+    // this.getMeals('beef');
 
     // // nutritionix queries
     // this.simpleGet()
     // this.nutritionGet()
-    this.nutrientsPost();
+    // this.getMealData("cup noodles");
   }
 
   // gets random meal from api can be our featured meal?
-  getRandomMeal = async () => {
-    try {
-      const data = await axios.get("https://www.themealdb.com/api/json/v1/1/random.php");
-      console.log(data);
-      console.log(data.data.meals[0]);
-      console.log(data.data.meals[0].strMeal);
-      // picture of thing pulled
-      console.log(data.data.meals[0].strMealThumb);
-    } catch (e) {
-      console.error(e);
-    }
-  }
+  // getRandomMeal = async () => {
+  //   try {
+  //     const data = await axios.get("https://www.themealdb.com/api/json/v1/1/random.php");
+  //     console.log(data);
+  //     console.log(data.data.meals[0]);
+  //     console.log(data.data.meals[0].strMeal);
+  //     // picture of thing pulled
+  //     console.log(data.data.meals[0].strMealThumb);
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // }
 
-  getMealOfType = async () => {
-    const URL = '/api/meal/choose';
+
+  getMeals = async (type) => {
+    const URL = `/api/meal/choose?type=${type}`;
     try {
       const data = await axios({
         url: URL,
@@ -52,14 +71,14 @@ class MealSelect extends Component {
   // use simple get to get specific meals for breakfast, lunch, dinner, snack
   // populate 5 options
   // when they click one of the options it runs it through nutritionix next option to pull nutrition info
-  nutrientsPost = async (mealSelected) => {
-    const URL = "/api/meal";
+  getMealData = async (mealSelected) => {
+    const URL = `/api/meal?meal=${mealSelected}`;
 
     try {
       const data = await axios({
         url: URL,
-        method: "POST",
-        data: {meal: mealSelected},
+        method: "GET",
+        // data: {meal: mealSelected},
       });
       console.log(data);
       // add total calories per ingredients found
@@ -97,10 +116,10 @@ class MealSelect extends Component {
           <Row>
             <h3>Lunch</h3>
             {/* lunch main course dropdown */}
-            <MainCourseDropdown />
+            <MainCourseDropdown onMealSelect={this.handleMealSelect}/>
           </Row>
           <Row>
-            <MealCarousel />
+            <MealCarousel/>
           </Row>
 
           {/* Dinner carousel */}
--- client/src/components/Partials/MainCourseDropDown/index.js
+++ client/src/components/Partials/MainCourseDropDown/index.js
@@ -1,49 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 
-const MainCourseDropdown = props => {
-  return (
-    <div className="dropdown">
-      <button
-        className="btn btn-secondary dropdown-toggle"
-        type="button"
-        id="dropdownMenuButton"
-        data-toggle="dropdown"
-        aria-haspopup="true"
-        aria-expanded="false"
-      >
-        Select Main Course
-      </button>
-      <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a className="dropdown-item" href="#">
-          Beef
-        </a>
-        <a className="dropdown-item" href="#">
-          Chicken
-        </a>
-        <a className="dropdown-item" href="#">
-          Goat
-        </a>
-        <a className="dropdown-item" href="#">
-          Lamb
-        </a>
-        <a className="dropdown-item" href="#">
-          Pasta
-        </a>
-        <a className="dropdown-item" href="#">
-          Pork
-        </a>
-        <a className="dropdown-item" href="#">
-          Seafood
-        </a>
-        <a className="dropdown-item" href="#">
-          Vegan
-        </a>
-        <a className="dropdown-item" href="#">
-          Vegetarian
-        </a>
+export default class MainCourseDropDown extends Component {
+  state = {
+    display: false,
+    categories: [
+      "Beef",
+      "Chicken",
+      "Goat",
+      "Lamb",
+      "Pasta",
+      "Pork",
+      "Seafood",
+      "Vegan",
+      "Vegetarian"
+    ],
+    choice: "",
+  };
+
+  handleButtonClick = () => {
+    this.setState({ display: !this.state.display });
+  };
+
+  handleMealClick = (index) => {
+    console.log(index);
+    this.props.onMealSelect(this.state.categories[index]);
+  };
+  
+  render() {
+    return (
+      <div className="dropdown">
+        <button
+          className="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+          onClick={this.handleButtonClick}
+        >
+          Select Main Course
+        </button>
+        <div
+          className="dropdown-menu"
+          aria-labelledby="dropdownMenuButton"
+          style={{ display: this.state.display ? "block" : "none" }}
+        >
+          {this.state.categories.map((category,index) => {
+            return (
+              // function defined that onclick returns index of meal clicked
+              <li onClick={()=>{this.handleMealClick(index)}} className="dropdown-item" key={index}>
+                {category}
+              </li>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default MainCourseDropdown;
+    );
+  }
+}
