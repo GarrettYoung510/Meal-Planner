9054a1e2e141ce080afbde842c185a69cbd92f15
Ryan Gelow <exisphere@gmail.com>
2019-10-09 12:54:56 -0700

    updated sign in page with sign up column

--- client/src/containers/Signin.js
+++ client/src/containers/Signin.js
@@ -5,6 +5,8 @@ import { connect } from "react-redux";
 import { signin } from "../actions";
 
 import Container from "./../components/Partials/Container/Container";
+import Row from "./../components/Partials/Row/Row";
+import Column from "./../components/Partials/Column/Column";
 
 class Signin extends Component {
   onSubmit = formValues => {
@@ -28,34 +30,44 @@ class Signin extends Component {
         <br />
         <h5>Please login to see your profile</h5>
         <br />
-      <form onSubmit={handleSubmit(this.onSubmit)}>
-        <fieldset>
-          <label>Email</label>
-          <br />
-          <Field
-            name="email"
-            type="text"
-            label="Email"
-            component={this.renderInput}
-            autoComplete="none"
-          />
-        </fieldset>
-        <br/>
-        <fieldset>
-          <label>Password</label>
-          <br />
-          <Field
-            name="password"
-            type="password"
-            label="password"
-            component={this.renderInput}
-            autoComplete="none"
-          />
-        </fieldset>
+        <Row>
+          <Column small="12" medium="6" large="5">
+            <form onSubmit={handleSubmit(this.onSubmit)}>
+              <fieldset>
+                <label>Email</label>
+                <br />
+                <Field
+                  name="email"
+                  type="text"
+                  label="Email"
+                  component={this.renderInput}
+                  autoComplete="none"
+                  />
+              </fieldset>
+              <br/>
+              <fieldset>
+                <label>Password</label>
+                <br />
+                <Field
+                  name="password"
+                  type="password"
+                  label="password"
+                  component={this.renderInput}
+                  autoComplete="none"
+                  />
+              </fieldset>
+              <br />
+              <button className="btn btn-lg btn-outline-warning">Sign In</button>
+              <div>{this.props.errorMessage}</div>
+            </form>
+          </Column>
+          <Column small="12" medium="6" large="5">
+            <p>Don't have a profile yet?</p>
+            <button className="btn btn-lg btn-outline-warning">Sign Up</button>
+            <br />
+          </Column>
+        </Row>
         <br />
-        <button className="btn btn-lg btn-outline-warning">Signin</button>
-        <div>{this.props.errorMessage}</div>
-      </form>
       </Container>
     );
   }
