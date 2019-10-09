1a801a25114b66c775ebeed686e7fbca75c956f4
Ryan Gelow <exisphere@gmail.com>
2019-10-09 12:52:10 -0700

    catching up

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express   = require('express');
 const morgan    = require('morgan');
 const mongoose  = require('mongoose');
 const cors      = require('cors');
-const path      = require('path')
+const path      = require('path');
+const dotenv = require('dotenv');
 
 const app = express();
 
 // Database setupx
-mongoose.connect('mongodb://localhost:auth/auth', { 
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:auth/auth', { 
   useNewUrlParser: true, 
   useCreateIndex: true
 });
